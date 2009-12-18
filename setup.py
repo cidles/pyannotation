049d79aab425d46208da74470ad3b639f4782446
Peter Bouda <pbouda@cidles.eu>
2009-12-18 16:02:26 +0000

    nomsg

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pyannotation',
-      version='0.1.1',
+      version='0.2.0',
       description='Python Linguistic Annotation Library',
       long_description='PyAnnotation is a Python Library to access and manipulate linguistically annotated corpus files. Supported file format is currently only Elan XML, with Kura XML and Toolbox files support planned for future releases. A Corpus Reader API is provided to support statistical analysis within the Natural Language Toolkit. ',
       author='Peter Bouda',
