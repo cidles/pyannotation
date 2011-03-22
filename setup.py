8cace402c8571b955e3f21ded236f6ad78218eef
Peter Bouda <pbouda@cidles.eu>
2011-03-22 10:48:32 +0000

    Minor change in package description in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='pyannotation',
       version='0.3.0',
       description='Python Linguistic Annotation Library',
-      long_description='PyAnnotation is a Python Library to access and manipulate linguistically annotated corpus files. Supported file format is currently only Elan XML, with Kura XML and Toolbox files support planned for future releases. A Corpus Reader API is provided to support statistical analysis within the Natural Language Toolkit. ',
+      long_description='PyAnnotation is a Python Library to access and manipulate linguistically annotated corpus files. Supported file format is currently Elan XML, Kura XML and Toolbox files. A Corpus Reader API is provided to support statistical analysis within the Natural Language Toolkit. ',
       author='Peter Bouda',
       author_email='pbouda@cidles.eu',
       url='http://www.cidles.eu/ltll/poio-pyannotation',
